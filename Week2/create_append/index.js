bbc6a14d7e50c1336e6ecc31396d7640ca623262
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-09-05 19:32:08 -0400

    commit

--- Week2/create_append/index.js
+++ Week2/create_append/index.js
@@ -1,3 +1,7 @@
 var newHeader = document.createElement("h1")
-newH1.textContent = "Welcome to my JS site"
-document.body.append(h1)
+newHeader.textContent = "Welcome to my JS site"
+document.body.append(newHeader)
+
+var newP = document.createElement("p")
+newP.textContent = "All of this was created with JavaScript"
+document.body.append(newP)
