a4a98e7c43da754775f240c11cb19c61ad4712c1
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-09-06 00:17:28 -0400

    commit

--- Week2/create_append/index.js
+++ Week2/create_append/index.js
@@ -1,22 +1,14 @@
-var newHeader = document.createElement("h1")
+var newHeader = document.createElement("h1");
 newHeader.textContent = "Welcome to my JS site"
-document.body.append(newHeader)
+document.body.append(newHeader);
+
+
 
 var newP = document.createElement("p")
 newP.textContent = "All of this was created with JavaScript"
 document.body.append(newP)
 
-var newList = document.createElement("ol");
+var myDiv = document.createElement("div");
+console.dir(myDiv);
 
-var listItem1 = document.createElement("li");
-listItem1.innerHTML = ("HTML");
-listItem1.appendChild(newList);
-
-var listItem2 = document.createElement("li")
-listItem2.innerHTML = ("Javascript");
-listItem2.appendChild(newList);
-
-var listItem3 = document.createElement("li");
-listItem3.innerHTML = ("DOM");
-listItem3.appendChild(newList);
 
