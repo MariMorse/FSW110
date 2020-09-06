f4278f8764596af5bb62d2c4d4830968763fc7f3
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-09-05 20:28:26 -0400

    commit

--- Week2/create_append/index.js
+++ Week2/create_append/index.js
@@ -5,3 +5,16 @@ document.body.append(newHeader)
 var newP = document.createElement("p")
 newP.textContent = "All of this was created with JavaScript"
 document.body.append(newP)
+
+let items = ['Happy', 'Sad', 'Sunshine', 'Rain'],
+
+ol = document.createElement('ol');
+
+document.getElementById('myOList').appendChild(ol);
+
+items.forEach(function (item) {
+    let li = document.createElement('li');
+    ol.appendChild(li);
+
+    li.innerHtml += item;
+});
