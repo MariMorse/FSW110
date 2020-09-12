0a60f111621c3c78ebab78e7bd7d5cc0f9c465f9
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-09-12 19:08:01 -0400

    commit

--- Week3/creating_a_lis/index.js
+++ Week3/creating_a_lis/index.js
@@ -1,27 +1,34 @@
-var hOne = document.createElement("h1");
-hOne.setAttribute("id", "hOneText")
-hOne.textContent = "Hello World";
-document.body.appendChild(hOne);
+//(\n***Question 1***)
 
-for (var i = 1; i < 10; i++) {
-var x = document.getElementById("hOneText")
+for (var i = 0; i < 10; i++) {
+    var hOne = document.createElement("h1");
+    hOne.setAttribute("id", "hOneText");
+    hOne.textContent = "Hello World";
+    document.body.appendChild(hOne);
+    hOne.style.color = "blue";
+    hOne.style.backgroundColor = "yellow";
 }
 
+//(/n***Question 2***)
 var myUL = document.createElement("ul");
-myUL.setAttribute("id", "nameUL");
-document.body.appendChild(myUL);
+myUL.setAttribute("id", "myArray")
+document.body.appendChild(myUL)
 
-//const names = [
-//     "steve",
-//     "larry",
-//     "joe",
-//     "shirley",
-//     "steph",
-//     "nate",
-//     "rick",
-//     "emily"
-//]
+var  names = [
+     "steve",
+     "larry",
+     "joe",
+     "shirley",
+     "steph",
+     "nate",
+     "rick",
+     "emily"
+]
 
-//var newName = document.createElement("li")
-//document.getElementById(nameUL);
-//myUL.append(newName)
+var namesList = document.getElementById("myArray")
+
+for (var i = 0; i < names.length; i++) {
+    var newName = document.createElement("li")
+    newName.textContent = names[i]
+    namesList.append(newName)
+}
