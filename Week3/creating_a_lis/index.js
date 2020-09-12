df080d3c66d3cc9eabb174b39b6c163de9ef70bd
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-09-12 11:34:57 -0400

    commit

---
+++ Week3/creating_a_lis/index.js
@@ -0,0 +1,27 @@
+var hOne = document.createElement("h1");
+hOne.setAttribute("id", "hOneText")
+hOne.textContent = "Hello World";
+document.body.appendChild(hOne);
+
+for (var i = 1; i < 10; i++) {
+var x = document.getElementById("hOneText")
+}
+
+var myUL = document.createElement("ul");
+myUL.setAttribute("id", "nameUL");
+document.body.appendChild(myUL);
+
+//const names = [
+//     "steve",
+//     "larry",
+//     "joe",
+//     "shirley",
+//     "steph",
+//     "nate",
+//     "rick",
+//     "emily"
+//]
+
+//var newName = document.createElement("li")
+//document.getElementById(nameUL);
+//myUL.append(newName)
