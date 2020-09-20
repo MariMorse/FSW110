74c997cd30611694e18d23ab1b65c2116993dc5a
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-09-20 15:24:26 -0400

    commit

--- Week4/calculatorJS/calculator.js
+++ Week4/calculatorJS/calculator.js
@@ -2,9 +2,11 @@ var hOne = document.createElement("h1")
 hOne.textContent = "Marianne's Javascript Calculator"
 document.body.append(hOne);
 
+function adding() {
 
-function additionBy() {
-        num1 = document.getElementById("value1").value;
-        num2 = document.getElementById("value2").value;
-        document.getElementById("result").innerHTML = num1 + num2;
+document.getElementById("first").value= " "
+document.getElementById("second").value= " ";
+document.getElementById("answer").value= " "; 
+
 }
+document.getElementById("add").addEventListener("click", adding);
