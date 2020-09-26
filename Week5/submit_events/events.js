cbfadde9015c1b809e6e190433d65e1ebac76fb9
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-09-26 14:38:27 -0400

    commit

---
+++ Week5/submit_events/events.js
@@ -0,0 +1,12 @@
+document.getElementById("myBtn").addEventListener("click", function(){
+    var alert1 = document.getElementById("fname");
+    var alert2 = document.getElementById("lname");
+    var alert3 = document.getElementById("email");
+    alert(alert1.value + " " + alert2.value + ", " + alert3.value)
+});
+
+
+
+
+
+    
