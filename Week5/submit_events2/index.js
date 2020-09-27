615e0d2bb9ee9102238940f2de3197b1dcb5d9a8
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-09-27 17:25:53 -0400

    commit

--- Week5/submit_events2/index.js
+++ Week5/submit_events2/index.js
@@ -1,15 +1,21 @@
-var fname = document.getElementById("fname");
-var lname = document.getElementById("lname");
-var fname = document.getElementById("age");
-var lname = document.getElementById("gender");
-var fname = document.getElementById("city");
-var lname = document.getElementById("diet");
+const form = form.passengerInfo
 
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
 
+function myalert() {
+    const first = form.fName.value;
+    const last = form.lName.value;
+    const age = form.age.value; 
+    const gender = form.gender.value; 
+    const cityselect = form.city.value;
 
-
-
-document.getElementById("namebutton").addEventListener("click", function () {
-
-
-    })        
+    const dietary = [];
+        for(let i = 0; i < form.diet.length; i++){
+            if(form.diet[i].checked){
+        dietary.push(form.diet[i].value)
+    }
+        }
+    alert(" First Name: " + "" +  first + " \nLast Name: " + "" + last + " \nAge: " + "" + age + " \nGender: " + "" + gender + " \nCity: " + "" + cityselect + " \nDietary Restrictions: " + "" + checkedInput)
+}
+});
