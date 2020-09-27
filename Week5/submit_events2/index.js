3ee1e4999253faa80ac0859e11aed77016707001
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-09-27 01:32:42 -0400

    commit

--- Week5/submit_events2/index.js
+++ Week5/submit_events2/index.js
@@ -1,13 +1,2 @@
-document.getElementById("myBtn").addEventListener("click", function(){
-    const firstN = form.fname.value;
-    const nameL = form.lname.value;
-    const ageNum = form.age.value;
-    const oneMale = form.gender.value; 
-    const 
-
-
-
-
-    alert(" First Name:" + "" + first + " Last Name:" + "" + last + " Age:" + "" + age + " Gender:" + "" + gender + " City:" + "" + cityselect + " Dietary Restrictions:" + "" + checkedInput)
-}
-});
+myFunction = () => (fname.value, lname.value, age.value, gender.value, city.value, dietRestrict.value !=="") ?
+alert("First name" + fname +)
