500f134adca5b4a73f955a5419c64633eac23555
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-10-02 17:12:01 -0400

    commit

--- Week6/events_workbook/mousemove/app.js
+++ Week6/events_workbook/mousemove/app.js
@@ -1,8 +1,6 @@
 let location = document.getElementsByClassName('red-box')[0]
-
-let x = event.clientX;
+location.addEventListener('mousemove', function(){
+let x = event.location.clientX;
 let y = event.clientY;
 
-location.addEventListener('mousemove', function(){
-    var location = "X coords: " + x + ", Y coords: " + y;
 });
