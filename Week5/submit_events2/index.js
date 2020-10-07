a203dda1db8674c184c591e9d6d9399df4a9c57c
Mari Morse <marianne.morse@bryanuniversity.edu>
2020-10-07 14:35:33 -0400

    commit

--- Week5/submit_events2/index.js
+++ Week5/submit_events2/index.js
@@ -4,20 +4,21 @@ document.getElementById("myBtn").addEventListener("click", function () {
     const age = document.getElementById("age");
     const gender = document.getElementById("gender");
     const city = document.getElementById("city");
-    
-    function getSelected() {
-        var restrictions = document.form.restrictions
-        var checkedBox = []
-        for (let i = 0; i < restrictions.length; i++) {
-            if (restrictions[i].checked) {
-            checkedBox.push(restrictions[i].value)
-            }
-        }
-        return checkedBox
-    }
-    alert(" First Name: " + "" + firstN.value + " \nLast Name: " + "" + lastN.value + " \nAge: " + "" + age.value + " \nGender: " + "" + gender.value + " \nCity: " + "" + city.value + " \nDietary Restrictions: " + "" + getSelected.restrictions)
+    const dietary = document.getElementById("diet");
+    alert(" First Name: " + "" + firstN.value + " \nLast Name: " + "" + lastN.value + " \nAge: " + "" + age.value + " \nGender: " + "" + gender.value + " \nCity: " + "" + city.value + " \nDietary Restrictions: " + "" + dietary.value)
 
 });
 
 
 
+/*function getSelected() {
+    var restrictions = document.form.restrictions
+    var checkedBox = []
+    for (let i = 0; i < restrictions.length; i++) {
+        if (restrictions[i].checked) {
+        checkedBox.push(restrictions[i].value)
+        }
+    }
+    return checkedBox
+}
+*/
